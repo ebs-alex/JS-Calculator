83b1e84237540ab801b083eb56fedcde34f993a5
Alex Moran <ebs.alex@icloud.com>
2023-11-05 21:22:43 -0800

    added relavnt event listeners for all buttons. inital setup of their function calls.

--- script.js
+++ script.js
@@ -1,15 +1,61 @@
-const title  = document.querySelector("#title")
-title.style.textDecoration = "underline"
-title.style.textUnderlineOffset = "4px"
-title.style.textDecorationThickness = "1px"
-title.textContent
+const title  = document.querySelector("#title");
+title.style.textDecoration = "underline";
+title.style.textUnderlineOffset = "4px";
+title.style.textDecorationThickness = "1px";
 
-const buttons = document.querySelectorAll("button")
+const currentEntry = document.querySelector(".current-entry");
+
+const numButtons = document.querySelectorAll(".number");
+numButtons.forEach( (button) => {
+    button.addEventListener('click', (event) => numSelection(event.target));
+});
+
+const operators = document.querySelectorAll(".operator");
+operators.forEach( (button) => {
+    button.addEventListener('click',(event) => operatorSelection(event.target))
+});
+
+const allClearBtn = document.querySelector("#all-clear");
+allClearBtn.addEventListener('click', allClear);
+
+const backspaceBtn = document.querySelector("#backspace");
+backspaceBtn.addEventListener('click', backspaceEvent);
+
+const pointBtn = document.querySelector("#point");
+pointBtn.addEventListener("click", pointSelection);
+
+const equalsBtn = document.querySelector("#equals");
+equalsBtn.addEventListener("click", equalsSelection);
 
 
-buttons.forEach( (button) => {
-    button.addEventListener('click', () => {
-        console.log(button.textContent)
-    })
-})
+
+
+
+
+////functions
+
+function numSelection(number) {
+    currentEntry.textContent += number.textContent;
+}
+
+function operatorSelection(operator) {
+    currentEntry.textContent += operator.textContent;
+}
+
+function pointSelection() {
+    console.log(".");
+};
+
+function equalsSelection() {
+    console.log("=");
+};
+
+function backspaceEvent() {
+    console.log("backspace")
+}
+
+function allClear() {
+    currentEntry.textContent = ""
+}
+
 
