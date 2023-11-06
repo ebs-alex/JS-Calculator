7f36ec98f90d7bcbb637bcdd02fc20e157679c02
Alex Moran <ebs.alex@icloud.com>
2023-11-05 16:48:41 -0800

    added event listener to all buttons that registers their textContent to the console

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 const title  = document.querySelector("#title")
 title.style.textDecoration = "underline"
+title.style.textUnderlineOffset = "4px"
+title.style.textDecorationThickness = "1px"
+title.textContent
+
+const buttons = document.querySelectorAll("button")
+
+
+buttons.forEach( (button) => {
+    button.addEventListener('click', () => {
+        console.log(button.textContent)
+    })
+})
+
